0c5e27e41083679917c4c38367b5823bc8335972
pengruolan <pengruolan@prl.com>
2020-11-27 15:17:21 +0800

    '分页对象'

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import cn.itcast.travel.domain.Category;
 import cn.itcast.travel.service.CategoryService;
 import cn.itcast.travel.util.JedisUtil;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +20,9 @@ public class CategoryServiceImpl implements CategoryService {
     public List<Category> findAll() {
         //1、查询redis
         Jedis jedis= JedisUtil.getJedis(); //用JedisUtils工具类获取jedis客户端
-        Set<String> zset = jedis.zrange("category", 0, -1); //使用sortedset排序查询，按cid排序
+        //Set<String> zset = jedis.zrange("category", 0, -1); //查询sortedset的值cname，按cid排序
+        Set<Tuple> zset = jedis.zrangeWithScores("category", 0, -1);//查询sortedset中的分数cid和值cname，按cid排序(header.html页面中需要携带cid值进行类别跳转: http://localhost/travel/route_list.html?cid=1)
+
         //2、判断查询集合是否为空
         List<Category> list = null ;
         if(zset == null || zset.size() == 0){
@@ -32,9 +35,15 @@ public class CategoryServiceImpl implements CategoryService {
         }else{
             //不为空，不是第一次访问，将zset的数据存入list
             list = new ArrayList<Category>();
-            for(String name : zset){
+            /*for(String name : zset){
                 Category category = new Category();
                 category.setCname(name);
+                list.add(category);
+            }*/
+            for(Tuple tuple : zset){
+                Category category = new Category();
+                category.setCname(tuple.getElement());
+                category.setCid((int)tuple.getScore());
                 list.add(category);
             }
         }
